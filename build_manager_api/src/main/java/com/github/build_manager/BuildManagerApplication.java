547dc17f651ddebc0582765afb49fbbc1cfcf458
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-09-25 12:28:43 -0300

    reorganization

---
+++ build_manager_api/src/main/java/com/github/build_manager/BuildManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.build_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildManagerApplication.class, args);
+	}
+
+}
