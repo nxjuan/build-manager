8950c0d142f76d5a43ce5c36a0b65f8b9f26617a
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-09-27 11:17:52 -0300

    using first funtional endpoint

---
+++ build_manager_api/src/main/java/com/github/build_manager/application/managers/ManagerController.java
@@ -0,0 +1,28 @@
+package com.github.build_manager.application.managers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/v1/manager")
+@Slf4j //faz com que possamos usar o log
+public class ManagerController {
+
+    @PostMapping
+    public ResponseEntity save
+    (
+        @RequestParam("name") String name,
+        @RequestParam("email") String email,
+        @RequestParam("password") String password,
+        @RequestParam(value = "photo", required = false)MultipartFile photo // a foto de perfil é opcional poivai existir a foto default do icone cinza
+    ){
+        log.info("dados recebidos: nome {}, email {}, senha {}", name, email, password);
+        return ResponseEntity.ok().build();
+    }
+
+}
