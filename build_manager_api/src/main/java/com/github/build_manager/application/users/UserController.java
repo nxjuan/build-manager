4e921db0df2ce416d158099067893c4e33f6bca8
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-09-27 12:09:42 -0300

    Postgres and docker stack created

---
+++ build_manager_api/src/main/java/com/github/build_manager/application/users/UserController.java
@@ -0,0 +1,12 @@
+package com.github.build_manager.application.users;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    // Adicionar o metodo authenticate, para que todos os usuários tenham que autenticar.
+
+}
