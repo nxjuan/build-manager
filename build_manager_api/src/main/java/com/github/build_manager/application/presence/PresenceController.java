55c7d3d252905684d76ebee89cbe3ee66829fcd4
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-10-09 14:28:00 -0300

    presence method 'update' implemented

--- build_manager_api/src/main/java/com/github/build_manager/application/presence/PresenceController.java
+++ build_manager_api/src/main/java/com/github/build_manager/application/presence/PresenceController.java
@@ -5,10 +5,7 @@ import com.github.build_manager.domain.service.PresenceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/presence")
@@ -23,5 +20,15 @@ public class PresenceController {
         Presence presence = presenceMapper.mapToPresence(dto);
         presenceService.save(presence);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{presence_id}")
+    public ResponseEntity<PresenceDTO> update(@PathVariable String presence_id, PresenceDTO dto){
+        Presence presenceToUpdate = presenceMapper.mapToPresence(dto);
+        presenceToUpdate.setId(presence_id);
+
+        Presence updatedPresence = presenceService.update(presenceToUpdate);
+
+        return ResponseEntity.ok().build();
     }
 }
--- build_manager_api/src/main/java/com/github/build_manager/application/presence/PresenceServiceImpl.java
+++ build_manager_api/src/main/java/com/github/build_manager/application/presence/PresenceServiceImpl.java
@@ -1,11 +1,15 @@
 package com.github.build_manager.application.presence;
 
 import com.github.build_manager.domain.entity.Presence;
+import com.github.build_manager.domain.exceptions.ResourceNotFoundException;
 import com.github.build_manager.domain.service.PresenceService;
 import com.github.build_manager.infra.repository.PresenceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +20,28 @@ public class PresenceServiceImpl implements PresenceService {
     @Transactional
     public Presence save(Presence presence) {
         return presenceRepository.save(presence);
+    }
+
+    @Override
+    public Presence update(Presence presence) {
+        Optional<Presence> existingPresence = presenceRepository.findById(presence.getId());
+
+        if(existingPresence.isPresent()){
+            Presence updatePresence = existingPresence.get();
+
+            updatePresence.setDate(presence.getDate());
+            updatePresence.setStart_time_work(presence.getStart_time_work());
+            updatePresence.setEnd_time_work(presence.getEnd_time_work());
+
+            Duration duration = Duration.between(
+                    presence.getStart_time_work(),
+                    presence.getEnd_time_work()
+            );
+
+            updatePresence.setDuration_time_work(duration);
+            return presenceRepository.save((updatePresence));
+        } else {
+            throw new ResourceNotFoundException("Presence not found with ID: " + presence.getId());
+        }
     }
 }
