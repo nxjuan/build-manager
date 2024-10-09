63d6ef8f7816665ed85572404db512ae9d41508c
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-10-09 15:04:36 -0300

    @JsonIgnore annotation added in Employee entity

--- build_manager_api/src/main/java/com/github/build_manager/domain/service/EmployeeService.java
+++ build_manager_api/src/main/java/com/github/build_manager/domain/service/EmployeeService.java
@@ -2,8 +2,12 @@ package com.github.build_manager.domain.service;
 
 import com.github.build_manager.domain.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     Employee save(Employee operation);
 
     Employee update(Employee employee);
+
+    List<Employee> findAllByBuildId(String build_id);
 }
