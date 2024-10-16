ad15956431c34ba1926f06e663ce58c9d6a46c23
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-10-16 19:24:43 -0300

    irrelevant changes to the date

--- build_manager_front_end/src/app/build/page.tsx
+++ build_manager_front_end/src/app/build/page.tsx
@@ -1,9 +1,18 @@
-import { HeaderBar } from '../../components/HeaderBar/HeaderBar'
+import HeaderBar from '../../components/HeaderBar'
+import BuildCard from '@/components/BuildCards';
+import React, { useState, useEffect } from 'react';
+
+interface Build {
+    name: string;
+    address: string;
+  }
 
 export default function build() {
     return (
         <main>
             <HeaderBar />
+            <BuildCard />
+
         </main>
     )
 }
