813c5e92dd3c1db399a676461675c43946964e29
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-10-18 16:51:00 -0300

    Main updated

--- build_manager_front_end/src/app/page.tsx
+++ build_manager_front_end/src/app/page.tsx
@@ -2,7 +2,6 @@ import HeaderBar from "../components/HeaderBar";
 import  BuildCard  from '../components/BuildCards'
 
 
-
 export default function Home() {
   return (
     <main className="bg-gray-700 min-h-screen py-4 px-20">  
