8aa671ac15feacfd9bb5199781859dea0d8cc10b
Juan Nogueira <juannogueiraxavier@gmail.com>
2025-01-02 19:24:21 -0300

    added patch cto cors config

--- build_manager_api/src/main/java/com/github/build_manager/application/config/SecurityConfig.java
+++ build_manager_api/src/main/java/com/github/build_manager/application/config/SecurityConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class SecurityConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // Permite requisições do seu frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    @Configuration
+    public class SecurityConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")  // Permite requisições do seu frontend
+                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                    .allowedHeaders("*");
+        }
     }
-}
